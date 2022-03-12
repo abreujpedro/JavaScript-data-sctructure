1009402691cd98fbae717f2eb2856e706e82c0ab
João Pedro Della Bianca Abreu <78066198+abreujpedro@users.noreply.github.com>
2022-03-11 22:03:46 -0300

    Update native sort console log

--- nativeSort.js
+++ nativeSort.js
@@ -1,3 +1,3 @@
-console.time("Insertion Sort");
+console.time("Native Sort");
 console.log([2, 5, 6 , 9 , 3, 7, 1, 8, 10, 4 ].sort((a,b) => a-b));
-console.timeEnd("Insertion Sort");
+console.timeEnd("Native Sort");
